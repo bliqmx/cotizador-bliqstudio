4bb3d59ad21c68a3393aba0ea63ade548a366dc6
bliqmx <fercorralv@gmail.com>
2025-10-21 08:27:03 -0700

    Add local configuration for database and app settings

---
+++ config.local.php
@@ -0,0 +1,12 @@
+<?php
+$CFG = [
+  'db_host' => 'localhost',
+  'db_name' => 'bliqstudio_cotizador',
+  'db_user' => 'bliqstudio_cotizador',
+  'db_pass' => 'Qq1q2w3e4**',
+  'app_url' => 'https://bliqstudio.mx/cotizador',
+  'app_name' => 'Bliq Quoter',
+  'timezone' => 'America/Mazatlan',
+  'brand_color' => '#28348F',
+  'brand_logo' => ''
+];
